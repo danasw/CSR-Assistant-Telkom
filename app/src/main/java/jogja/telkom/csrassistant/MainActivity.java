baca3ec1e1831472e7a86280ec588d154c86b02c
Unknown <danasetyaji@gmail.com>
2019-01-09 15:51:16 +0700

    Main Menu

--- app/src/main/java/jogja/telkom/csrassistant/MainActivity.java
+++ app/src/main/java/jogja/telkom/csrassistant/MainActivity.java
@@ -2,6 +2,10 @@ package jogja.telkom.csrassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView add      = (CardView) findViewById(R.id.add_on);
+        CardView bill     = (CardView) findViewById(R.id.bill);
+        CardView upgrade  = (CardView) findViewById(R.id.up_down);
+        CardView unlock   = (CardView) findViewById(R.id.unlock);
+        CardView problem  = (CardView) findViewById(R.id.problem);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Add On Clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Tagihan Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        upgrade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Upgrade dan Downgrade Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        unlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Buka Isolir Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        problem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Kendala dan Error Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
